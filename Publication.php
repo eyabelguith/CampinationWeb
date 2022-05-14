33efcd5e8ffecae0795f46ac782313eed52aef95
eyabelguith <61548540+eyabelguith@users.noreply.github.com>
2022-05-15 01:27:02 +0200

    Add files via upload

---
+++ Publication.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Publication
+ *
+ * @ORM\Table(name="publication")
+ * @ORM\Entity
+ */
+class Publication
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idP", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idp;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=100, nullable=false)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=250, nullable=false)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="photo", type="string", length=100, nullable=false)
+     */
+    private $photo;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_Camper", type="integer", nullable=false)
+     */
+    private $idCamper;
+
+    public function getIdp(): ?int
+    {
+        return $this->idp;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getIdCamper(): ?int
+    {
+        return $this->idCamper;
+    }
+
+    public function setIdCamper(int $idCamper): self
+    {
+        $this->idCamper = $idCamper;
+
+        return $this;
+    }
+
+
+}
